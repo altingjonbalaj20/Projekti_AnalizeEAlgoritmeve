fe07b1d1bc19280c62d18929a9f8c2431f5095ef
altingjonbalaj20 <gjonbalaj2017@gmail.com>
2021-11-27 22:09:40 +0100

    shtimi i kostos dhe rrugeve tek Section

--- model/Section.java
+++ model/Section.java
@@ -1,13 +1,26 @@
 package model;
 
+import java.util.HashMap;
+
 public class Section {
     private String sectionName;
     private int xPosition;
     private int yPosition;
+    private HashMap<Section, Integer> paths;
+
     public Section(String sectionName, int xPosition, int yPosition){
         this.sectionName = sectionName;
+        paths = new HashMap<>();
         this.xPosition = xPosition;
         this.yPosition = yPosition;
+    }
+
+    public void addPath(Section section, int pathCost){
+        paths.put(section, pathCost);
+    }
+
+    public int getCost(Section section){
+        return (int) (paths.get(section));
     }
 
     public String getSectionName(){
